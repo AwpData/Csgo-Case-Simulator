868569db83ae3e3b5c13f9e25dbf7d049f535469
AwpData <awpdata@gmail.com>
2019-06-16 13:44:50 -0700

    Update and rename DefaultCase.java to ReferenceCase.java

--- ReferenceCase.java
+++ ReferenceCase.java
@@ -1,8 +1,8 @@
 
-public class DefaultCase {
+public class ReferenceCase {
 	protected int white, lightblue, blue, purple, pink, red, gold;
 
-	public DefaultCase() {
+	public ReferenceCase() {
 		this.white = 0;
 		this.lightblue = 0;
 		this.blue = 0;
@@ -14,32 +14,32 @@ public class DefaultCase {
 
 	protected void spin() { // Example Only
 		int percent = (int) ((int) 100 * Math.random() + 1);
-		if (percent >= 1 && percent <= 40) {
+		if (percent >= 0 && percent <= 40) {
 			System.out.print("White aquired");
 			this.white++;
-		} else if (percent >= 41 && percent <= 60) {
+		} else if (percent > 40 && percent <= 60) {
 			System.out.print("Lightblue acquired");
 			this.lightblue++;
-		} else if (percent >= 61 && percent <= 80) {
+		} else if (percent > 60 && percent <= 80) {
 			System.out.print("Blue acquired");
 			this.blue++;
-		} else if (percent >= 81 && percent <= 90) {
+		} else if (percent > 80 && percent <= 90) {
 			System.out.print("Purple acquired");
 			this.purple++;
-		} else if (percent >= 91 && percent <= 96) {
+		} else if (percent > 90 && percent <= 96) {
 			System.out.print("Pink acquired");
 			this.pink++;
-		} else if (percent >= 97 && percent <= 99) {
+		} else if (percent > 96 && percent <= 99) {
 			System.out.print("Red acquired");
 			this.red++;
-		} else if (percent == 100) {
+		} else if (percent > 99 && percent <= 101) {
 			System.out.print("Gold acquired");
 			this.gold++;
 		}
 	}
 
-	public void getWinnings() {
-		System.out.println("Winnings today: "); // Set # of new skins and # of duplicate skins found (based in text file)
+	public void getSessionWinnings() {
+		System.out.println("Winnings this session: "); // Set # of new skins and # of duplicate skins found (based in text file)
 		System.out.println("White (Consumer Grade): " + getWhite());
 		System.out.println("LightBlue (Industrial Grade): " + getLightBlue());
 		System.out.println("Blue (Mil-Spec): " + getBlue());
@@ -47,6 +47,11 @@ public class DefaultCase {
 		System.out.println("Pink (Classified): " + getPink());
 		System.out.println("Red (Covert): " + getRed());
 		System.out.println("Gold (Exceedingly Rare): " + getGold());
+		System.out.println("Credits: " + CreditsCases.getCreditsWin());
+	}
+	
+	public void getTotalWinnings() {
+		
 	}
 
 	public int getWhite() {
