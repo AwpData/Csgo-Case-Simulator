bbca16504fa743b36c18b73c6ec60d06e90b8e8f
AwpData <awpdata@gmail.com>
2019-06-17 17:58:52 -0700

    Update HelperMethods.java

--- HelperMethods.java
+++ HelperMethods.java
@@ -8,11 +8,12 @@ public class HelperMethods extends ClientInterface { // For Client Interface Cla
 		System.out.println("Which case would you like to buy? (Enter number) \n");
 		System.out.println("1. CSGO Weapons Case");
 		System.out.println("2. CSGO Weapons Case 2");
-		System.out.println("3. Quit program\n");
+		System.out.println("3. Small Credits Case");
+		System.out.println("4. Quit program\n");
 		return input.nextLine();
 	}
 
-	public static boolean buyCheck(String boxname, int cost, int numofboxes, int credits, String casename) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public static boolean buyCheck(String boxname, int cost, int numofboxes, int credits, String casename, String type) {
 		boolean buy = false;
 		while (!buy) {
 			int spendingtotal = numofboxes * cost;
@@ -30,26 +31,48 @@ public class HelperMethods extends ClientInterface { // For Client Interface Cla
 				System.out.println("Transaction successful: " + credits + " - " + spendingtotal);
 				credits -= spendingtotal;
 				System.out.println("Your credit balance is now: " + credits + " credits");
-				HelperMethods.spin(numofboxes, casename, "case");
+				HelperMethods.spin(numofboxes, casename, type);
 				return true;
 			}
 		}
 		return false;
 	}
 
-	public static void spin(int numofboxes, String casename, String boxtype) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		CsgoCases csgocase = new CsgoCases();
-		for (int i = 1; i <= numofboxes; i++) {
-			System.out.println("\nPress enter to open a " + boxtype + ": " + (numofboxes - i + 1) + " " + boxtype + "s remaining");
-			input.nextLine();
-			System.out.print(boxtype + " " + i + " contains: ");
-			Method method = csgocase.getClass().getMethod(casename);
-			method.invoke(csgocase);
+	public static void spin(int numofboxes, String casename, String boxtype) {
+		if (casename.contains("Credits")) { // Credits cases type spins
+			CreditsCases creditscase = new CreditsCases();
+			for (int i = 1; i <= numofboxes; i++) {
+				System.out.println("\nPress enter to open a " + boxtype + ": " + (numofboxes - i + 1) + " " + boxtype + "s remaining");
+				input.nextLine();
+				System.out.print(boxtype + " " + i + " contains: ");
+				try {
+					Method method = creditscase.getClass().getMethod(casename);
+					method.invoke(creditscase);
+				} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					System.out.println("ProgramError: How did this happen?");
+				}
+				System.out.println();
+			}
+			System.out.println();
+			creditscase.getSessionWinnings();
+			System.out.println();
+		} else { // Case type spins
+			CsgoCases csgocase = new CsgoCases();
+			for (int i = 1; i <= numofboxes; i++) {
+				System.out.println("\nPress enter to open a " + boxtype + ": " + (numofboxes - i + 1) + " " + boxtype + "s remaining");
+				input.nextLine();
+				System.out.print(boxtype + " " + i + " contains: ");
+				try {
+					Method method = csgocase.getClass().getMethod(casename);
+					method.invoke(csgocase);
+				} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					e.printStackTrace();
+				}
+				System.out.println();
+			}
+			System.out.println();
+			csgocase.getSessionWinnings();
 			System.out.println();
 		}
-		System.out.println();
-		csgocase.getSessionWinnings();
-		System.out.println();
 	}
-
 }
